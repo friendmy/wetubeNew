19cce12577f0cadb394bafd38ad171f95c58ebb6
friendmy <friendmytrue@gmail.com>
2019-09-30 21:45:42 +0900

    fighting

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,13 +1,15 @@
 import express from "express";
 import routes from "../routes";
-import { login, join, logout } from "../controllers/userController";
+import { getJoin, postJoin, logout, getLogin, postLogin } from "../controllers/userController";
 import { search, home } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
-globalRouter.get(routes.join, join);
-globalRouter.get(routes.login, login);
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
 globalRouter.get(routes.logout, logout);
 globalRouter.get(routes.search, search);
 
