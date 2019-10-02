793fac0c02473e7a66236d8b6e79e7d89a252d08
friendmy <friendmytrue@gmail.com>
2019-10-03 08:39:40 +0900

    mongodb complete

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,19 +6,20 @@ export const getJoin = (req, res) => {
 
 export const postJoin = (req, res) => {
   const {
-    body: { name, email, password, password2}
+    body: { password, password2 }
   } = req;
-  if (password !== password2){
+  if (password !== password2) {
     res.status(400);
   } else {
     // To Do: Register User
     // To Do: Log user in
     res.redirect(routes.home);
   }
-  res.render("join", { pageTitle: "Join"});
+  res.render("join", { pageTitle: "Join" });
 };
 
-export const getLogin = (req, res) => res.render("login", {pageTitle: "Log In"});
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Log In" });
 export const postLogin = (req, res) => {
   res.redirect(routes.home);
 };
@@ -28,8 +29,11 @@ export const logout = (req, res) => {
   res.redirect(routes.home);
 };
 
-export const userDetail = (req, res) => res.render("userDetail", {pageTitle: "User Detail"});
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "User Detail" });
 
-export const editProfile = (req, res) => res.render("editProfile", {pageTitle: "Edit Profile"});
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "Edit Profile" });
 
-export const changePassword = (req, res) => res.render("changePassword", {pageTitle: "Change Password"});
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "Change Password" });
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,21 +1,29 @@
-import express from "express";
-import routes from "../routes";
+import express from 'express';
+import routes from '../routes';
 import {
-	upload,
-	videoDetail,
-	editVideo,
-	deleteVideo,
-	getUpload,
-	postUpload
-} from "../controllers/videoController";
-import { uploadVideo } from "../middlewares";
+  videoDetail,
+  deleteVideo,
+  getUpload,
+  postUpload,
+  getEditVideo,
+  postEditVideo,
+} from '../controllers/videoController';
+import { uploadVideo } from '../middlewares';
 
 const videoRouter = express.Router();
 
+// console.log(routes.editVideo());
+
+// Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
